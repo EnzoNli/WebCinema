e3a9b26957e18299760f4cca1df5fada94872313
Zoé Marquis <zoe_marquis@ens.univ-artois.fr>
2023-02-07 19:18:43 +0100

    :lipstick: petites personnalisations des pages de base

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="co-authored by enzo nulli, zoé marquis">
     <link rel="stylesheet" href="index.css">
-    <title>Document</title>
+    <link rel="icon" type="image/png" href="logo.png"/>
+    <title>Accueil</title>
 </head>
 <body>
     <header>
--- login.php
+++ login.php
@@ -35,12 +35,14 @@ if (isset($_POST['submit'])) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="co-authored by enzo nulli, zoé marquis">
     <link rel="stylesheet" href="login.css">
+    <link rel="icon" type="image/png" href="logo.png"/>
     <title>Connexion</title>
 </head>
   <body>
