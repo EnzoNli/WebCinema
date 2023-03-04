d3a0e66b1bca19a1c866b6cda86f1e13c260b38e
Zoé Marquis <zoe_marquis@ens.univ-artois.fr>
2023-03-04 17:27:00 +0100

    premier formulaire et fc utiles

--- include/fcs.php
+++ include/fcs.php
@@ -2,7 +2,7 @@
 
 require_once("db_connexion.php");
 
-$connexion = new ConnexionDB("");
+$connexion = new ConnexionDB("../database");
 
 // 1 filtrer
 
@@ -45,23 +45,59 @@ function filtrer_annee($debut, $fin) {
     return $st->fetchAll();
 }
 
+// verifier que l'annee correspondant à debut <= annee fin
+// sinon ??
+
 // dans un autre fichier avec help pour afficher html
+
+function afficher_form() {
+    $chaine = '<form id="form" action="????.php" method="post">
+    ';
+    $chaine .= afficher_select_trier();
+    $chaine .= '<input type="text" name="titre" id="titre" placeholder="Titre de film">
+    ';
+    $chaine .= afficher_select_annee("debut");
+    $chaine .= afficher_select_annee("fin");
+    $chaine .= afficher_select_genre();
+    $chaine .= '<input type="text" name="acteur" id="acteur" placeholder="Acteur">
+    ';
+    $chaine .= '<br>
+                <input type="submit" name="submit" value="Rechercher">
+                </form>
+                ';
+    return $chaine;
+}
+
 function afficher_select_trier() {
-    $chaine = '<select name="trier" id="trier" title="Trier par">\n';
-    $chaine .= '<option value="+populaire">Les plus populaires</option>\n';
-    $chaine .= '<option value="-populaire">Les moins populaires</option>\n';
-    $chaine .= '<option value="+notes">Les mieux notés</option>\n';
-    $chaine .= '<option value="-notes">Les moins bien notés</option>\n';
-    $chaine .= '<option value="+date">Les plus récents</option>\n';
-    $chaine .= '<option value="-date">Les moins récents</option>\n';
-    $chaine .= '<option value="+titre">Ordre alphabétique du titre</option>\n';
-    $chaine .= '</select>\n';
+    $chaine = '<select name="trier" id="trier" title="Trier par">
+    ';
+    $chaine .= '<option value="sans" selected> -- Trier par -- </option>
+    ';
+    $chaine .= '<option value="+populaire">Les plus populaires</option>
+    ';
+    $chaine .= '<option value="-populaire">Les moins populaires</option>
+    ';
+    $chaine .= '<option value="+notes">Les mieux notés</option>
+    ';
+    $chaine .= '<option value="-notes">Les moins bien notés</option>
+    ';
+    $chaine .= '<option value="+date">Les plus récents</option>
+    ';
+    $chaine .= '<option value="-date">Les moins récents</option>
+    ';
+    $chaine .= '<option value="+titre">Ordre alphabétique du titre</option>
+    ';
+    $chaine .= '</select>
+    ';
     return $chaine;
 }
 
 function afficher_select_genre() {
     global $connexion;
-    $chaine = '<select name="genre" id="genre" title="Sélectionner un genre">\n';
+    $chaine = '<select name="genre" id="genre" title="Sélectionner un genre">
+    ';
+    $chaine .= '<option value="sans" selected> -- Genre -- </option>
+    ';
     $sql = 'SELECT nom_genre FROM Genre ORDER BY nom_genre';
     $result = $connexion->getDB()->query($sql);
     while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
@@ -69,11 +105,27 @@ function afficher_select_genre() {
         $chaine .= $row['nom_genre'];
         $chaine .= '">';
         $chaine .= $row['nom_genre'];
-        $chaine .= '</option>\n';
+        $chaine .= '</option>
+        ';
     }
-    $chaine .= '</select>\n';
+    $chaine .= '</select>
+    ';
     return $chaine;
 }
 
 function afficher_select_annee($marqueur) { // réutiliser avec "debut" et "fin" (entre ... et ...)
+    // de 1895:défaut à 2023:défaut
+    $an = 2023;
+    $chaine = '<select name="' . $marqueur . '" id="' . $marqueur . '" title="Trier par">
+    ';
+    while ($an > 1894) {
+        $chaine .= '<option value="' . $an . '">' . $an . '</option>
+        ';
+        $an -= 1;
+    }
+    $chaine .= '</select>
+    ';
+    return $chaine;
 }
+
+echo afficher_form();
