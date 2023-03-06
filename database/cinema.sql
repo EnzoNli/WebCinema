95301b58ade26271a1859a3f0dbfc75227066fe2
Zoé Marquis <zoe_marquis@ens.univ-artois.fr>
2023-03-06 15:27:41 +0100

    on peut maintenant noter un film

--- database/cinema.sql
+++ database/cinema.sql
@@ -2,12 +2,6 @@ pragma foreign_keys = true;
 PRAGMA encoding="UTF-8";
 PRAGMA case_sensitive_like = true;
 
-
--- view
--- triggers
--- transaction
-
-
 -- entités
 create table Utilisateur(
 	login_  varchar(20)		check(length(login_)>=3)    primary key,
@@ -34,7 +28,7 @@ create table Genre(
 create table Noter(
     login_      varchar(20) references Utilisateur(login_),
     api_movie_id     integer     references Film(api_movie_id),     
-    note        integer     check(note >=0 and note<=5)     not null,
+    note        integer     check(note >=0 and note<=10)     not null,
     commentaire varchar(5000),
     constraint pkNoter primary key (login_, api_movie_id)
 );
@@ -58,99 +52,41 @@ create view NoteMoyenne as
     group by api_movie_id
 ;
 
-create view MeilleureNotes as 
-    select api_movie_id, moyenne -- pour "écrire 4.7/5"
-    from NoteMoyenne
-    order by moyenne desc
-;
-
 create view NbNotes as 
     select count(note) as nb, api_movie_id
     from Noter
     group by api_movie_id
 ;
 
--- utile ?
-create view PopulariteDecroissant as
-    select api_movie_id, nb -- pour écrire "30 avis"
-    from NbNotes
-    order by nb desc
-;
-
--- utile ?
-create view PopulariteCroissant as
-    select api_movie_id, nb
-    from NbNotes
-    order by nb asc
-;
-
--- date de sortie
-
-/* create trigger EmpecheAjoutNote
-before insert on Noter
-begin 
-    -- si le film existe dans la db
-    -- si le film n'existe pas
-    -- si le login a déjà donné une note
-    -- ...
-    select 
-        case 
-            when (new.id_film not in Film)
-            then -- ajouter le film à la db
-
-            when (  select login_, id_film
-                    from Noter
-                    where new.id_film=id_film and new.login_ = login_)
-            then raise(abort, 'Vous avez déjà noté ce film !')
-        end;
-end; */
-
--- pour essayer d'inserer dasns noter, faut déjà avoir insérer dans film 
--- donc tout ça c'est une transaction
-
-
-create table Film(
-    api_movie_id     integer    primary key,
-    titre       varchar(50)     not null,
-    date_sortie date     not null
-
+/*
 BEGIN TRANSACTION;
-    IF NOT EXISTS(SELECT api_movie_id FROM Film WHERE api_movie_id = :movie_key)
-        BEGIN
-            INSERT INTO Film (api_movie_id, titre, date_sortie) 
-            VALUES (:movie_key, :titre_film, :date_sortie)
 
-            -- pour chaque genre
-            IF NOT EXISTS(SELECT api_genre_id FROM Genre WHERE api_genre_id = :genre_key)
-                BEGIN
-                    INSERT INTO Genre (api_genre_id, nom_genre) 
-                    VALUES (:genre_key, :nom_genre)
-                END
+INSERT into Film (api_movie_id, titre, date_sortie) 
+select 666, "KLOU ET LES OVNI", date("2021-12-01")
+WHERE NOT EXISTS (SELECT 1 FROM Film WHERE api_movie_id = 666);
+        
+INSERT INTO Genre (api_genre_id, nom_genre) 
+SELECT 11,"spatiale"
+WHERE NOT EXISTS (SELECT 1 FROM Genre WHERE api_genre_id = 11);
 
-            INSERT INTO Appartenir (api_genre_id, api_movie_id)
-            VALUES (:genre_key, :movie_key)
-            -- fin pour chaque genre
+INSERT INTO Appartenir (api_genre_id, api_movie_id) 
+select 11,666 
+WHERE NOT EXISTS (SELECT 1 FROM Appartenir WHERE api_movie_id = 666 AND api_genre_id = 11);
 
-            -- pour chaque acteur
-            IF NOT EXISTS(SELECT api_acteur_id FROM Acteur WHERE api_acteur_id = :acteur_key)
-                BEGIN
-                    INSERT INTO Acteur (api_acteur_id, nom_acteur) 
-                    VALUES (:acteur_key, :nom_acteur)
-                END
-
-            INSERT INTO Jouer (api_acteur_id, api_movie_id) 
-            VALUES (:acteur_key,:movie_key)
-            -- fin pour chaque acteur
-        END
-
-    -- insérer notes
-    IF NOT EXISTS (SELECT * FROM Noter WHERE api_movie_id = :movie_key AND login_ = :login_)
-        BEGIN 
-            INSERT INTO Noter (login_, api_movie_id, note, commentaire)
-            VALUES (:login_, :api_movie_id, :note, :commentaire)
-        END
+INSERT INTO Acteur (api_acteur_id, nom_acteur) 
+SELECT 3, "Olak" 
+WHERE NOT EXISTS (SELECT 1 FROM Acteur WHERE api_acteur_id = 3);
+      
+INSERT INTO Jouer (api_acteur_id, api_movie_id) 
+select 3, 666
+WHERE NOT EXISTS (SELECT 1 FROM Jouer WHERE api_movie_id = 666 AND api_acteur_id = 3);
+  
+INSERT INTO Noter (login_, api_movie_id, note, commentaire) 
+SELECT "Zoze", 666, 8, "Très bien"
+WHERE NOT EXISTS (SELECT 1 FROM Noter WHERE api_movie_id = 666 AND login_ = "Zoze");
 
 COMMIT;
+*/
 
 
 INSERT INTO Genre (api_genre_id, nom_genre)
